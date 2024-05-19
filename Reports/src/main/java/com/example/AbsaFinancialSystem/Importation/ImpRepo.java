e9a953c81c6f9d8a4c385f8f5224cebd0a1adb20
Kipkem <kipkemboiarnold22@gmail.com>
2024-05-19 11:36:40 +0300

    Kipkem

---
+++ Reports/src/main/java/com/example/AbsaFinancialSystem/Importation/ImpRepo.java
@@ -0,0 +1,11 @@
+package com.example.AbsaFinancialSystem.Importation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface ImpRepo extends JpaRepository<Imp, Long> {
+}
+
